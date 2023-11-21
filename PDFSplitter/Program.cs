80f8b8f0b6ac37c824ddf884b8e15be3dce2f01e
Harsh K <hersh.k17@gmail.com>
2023-11-21 18:58:10 -0500

    added splitter by bookmarks

--- PDFSplitter/Program.cs
+++ PDFSplitter/Program.cs
@@ -1,39 +1,71 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
 
-namespace PdfSplitter
+public class PDFSplitter
 {
-    public class Class1
+    public static void Main(string[] args)
     {
-        public static void Main()
+        string inputFilePath = @"C:\Users\hersh\Documents\Projects\cpp\PDFSplitter\PDFSplitter\example.pdf";
+        string outputDirectory = @"C:\Users\hersh\Documents\Projects\cpp\PDFSplitter\PDFSplitter\output\";
+
+        using (PdfReader reader = new PdfReader(inputFilePath))
         {
-            // The path of the source PDF file
-            string sourcePath = @"C:\Users\hersh\Downloads\sample.pdf";
-            // The path of the output folder
-            string outputPath = @"C:\Users\hersh\Downloads\Output";
+            // Retrieve all the bookmarks
+            var bookmarks = SimpleBookmark.GetBookmark(reader);
 
-            // Open the source PDF file as a PdfReader
-            using (PdfReader reader = new PdfReader(sourcePath))
+            if (bookmarks != null)
             {
-                // Get the number of pages in the source PDF file
-                int pageCount = reader.NumberOfPages;
-
-                // Loop through the pages of the source PDF file
-                for (int i = 1; i <= pageCount; i++)
+                // Iterate through each bookmark
+                foreach (var bookmark in bookmarks)
                 {
-                    // Create a new document for the output PDF file
-                    Document document = new Document();
-                    // Create a new PdfCopy for copying pages
-                    PdfCopy copy = new PdfCopy(document, new FileStream(outputPath + i + ".pdf", FileMode.Create));
-                    // Open the document
-                    document.Open();
-                    // Copy the current page from the source PDF file to the output PDF file
-                    copy.AddPage(copy.GetImportedPage(reader, i));
-                    // Close the document
-                    document.Close();
+                    // Split the bookmark and its children recursively
+                    SplitBookmark(reader, bookmark, "", outputDirectory);
                 }
+            }
+        }
+    }
+
+    public static string RemoveSpecialCharacters(string str)
+    {
+        StringBuilder sb = new StringBuilder();
+        foreach (char c in str)
+        {
+            if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
+            {
+                sb.Append(c);
+            }
+        }
+        return sb.ToString();
+    }
+
+    private static void SplitBookmark(PdfReader reader, IDictionary<string, object> bookmark, string parentTitle, string outputDirectory)
+    {
+        // Get the title and page number of the bookmark
+        string title = (string)bookmark["Title"];
+        int pageNumber = int.Parse(((string)bookmark["Page"]).Split(' ')[0]);
+
+        // Combine the parent title and current title to create a unique filename
+        string fileName = RemoveSpecialCharacters(title);
+
+        // Extract the page from the PDF
+        Document document = new Document();
+        PdfCopy copy = new PdfCopy(document, new FileStream(Path.Combine(outputDirectory, $"{fileName}.pdf"), FileMode.Create));
+        document.Open();
+        copy.AddPage(copy.GetImportedPage(reader, pageNumber));
+        document.Close();
+
+        // Check if the bookmark has children
+        if (bookmark.ContainsKey("Kids"))
+        {
+            var children = (System.Collections.Generic.List<System.Collections.Generic.Dictionary<string, object>>)bookmark["Kids"];
+            foreach (var child in children)
+            {
+                // Split the child bookmark recursively with the current title as the parent title
+                SplitBookmark(reader, child, fileName, outputDirectory);
             }
         }
     }
